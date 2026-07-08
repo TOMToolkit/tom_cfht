27fb8d6f119cc5918fe009d7866a08a09e6fe12c
William (Lindy) Lindstrom <llindstrom@lco.global>
2026-07-08 12:08:44 -0700

    fix copy-paste typo

--- tom_cfht/urls.py
+++ tom_cfht/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ProfileUpdateView
 
 
-app_name = 'tom_keck'
+app_name = 'tom_cfht'
 
 urlpatterns = [
     path('users/<int:pk>/update/', ProfileUpdateView.as_view(), name='cfht-profile-update'),
